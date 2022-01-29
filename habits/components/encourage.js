b4d05329e5a81b9dcad790471d67b21d4927a77e
Vincent Luciani <vincent.luciani@schneider-electric.com>
2022-01-29 12:18:29 +0100

    work on encouragement phrases

--- habits/components/encourage.js
+++ habits/components/encourage.js
@@ -4,7 +4,8 @@ var encourageIfPassedTarget = function(result, target){
 
         if ( result == target){
             document.getElementById("positive-message").style.display="flex";
-            document.getElementById("positive-message-title").innerHTML = buildCongratulationMessage('en_US')+" :)";
+            document.getElementById("positive-message-title").innerHTML = buildCongratulationTitle('en_US')+" :)";
+            document.getElementById("positive-message-subtitle").innerHTML = buildCongratulationSubTitle('en_US') ;
         }
 }
 
@@ -12,10 +13,18 @@ var closeMessage = function(){
     document.getElementById("positive-message").style.display="none";
 }
 
-var buildCongratulationMessage = function (locale){
-    var listOfWonderfulWords = translations['en_US'].amazing;
+var buildCongratulationTitle = function (locale){
+    var listOfWonderfulWords = translations[locale].amazing;
 
     var randomNumber = getRandomNumber(0, listOfWonderfulWords.length - 1);
 
     return listOfWonderfulWords[randomNumber];
+}
+
+var buildCongratulationSubTitle = function (locale){
+    var listOfEncouragements = translations[locale].encouraging;
+
+    var randomNumber = getRandomNumber(0, listOfEncouragements.length - 1);
+
+    return listOfEncouragements[randomNumber];
 }
