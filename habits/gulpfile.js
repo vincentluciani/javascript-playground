f6aa2eaf779a9da77b66616675b82a3eed092701
Vincent Luciani <vincent.luciani@schneider-electric.com>
2022-02-12 08:20:54 +0100

    gulp processing

--- habits/gulpfile.js
+++ habits/gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 const minify = require('gulp-minify');
+var cleanCss = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
+var rename = require('gulp-rename');
+var replace = require('gulp-replace');
+var fs = require('fs');
 
 gulp.task('pack-js', function () {    
     return gulp.src(['components/*.js', 'libraries/*.js', 'language/general.js','language/english.js', 'synchronization/*.js','habits_main.js'])
@@ -9,5 +14,20 @@ gulp.task('pack-js', function () {
         .pipe(gulp.dest('output'));
 });
  
+gulp.task('pack-css', function () {    
+    return gulp.src(['components/*.css'])
+        .pipe(concat('bundle.css'))
+        .pipe(cleanCss())
+        .pipe(gulp.dest('output'));
+});
 
-gulp.task('default', gulp.series('pack-js'));
+gulp.task('process-html', function () {    
+    return gulp.src(['adding_habits.html'])
+        .pipe(replace('<script type="text/javascript" src="output/bundle-min.js"></script>', '<script>'+fs.readFileSync('output/bundle-min.js', 'utf8')+'</script>'))
+        .pipe(replace('<link rel="stylesheet" href="components/full.css">', '<style>'+fs.readFileSync('output/bundle.css', 'utf8')+'</style>'))   
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('output'));
+});
+
+gulp.task('default', gulp.series(['pack-js','pack-css','process-html']));
