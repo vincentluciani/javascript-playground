da36d968c110225a0de071ad61bd921bcbd5634a
Vincent Luciani <vincent.luciani@schneider-electric.com>
2022-02-13 16:30:08 +0100

    Better positive messages

--- habits/language/english.js
+++ habits/language/english.js
@@ -1,5 +1,5 @@
 translations['en_US']  = {
 'amazing':['Amazing','Superb','Outstanding','Magnificient','Exceptional','Marvellous','Wonderful','Sublime','Supreme','Splendid','Fantastic','Awesome','Mind-blowing','Brilliant','Smashing'],
-'encouraging':['Everything you need to accomplish your goals is already in you!','Be gentle with yourself, you are doing the best you can!','A little progress each day adds up to big results!','You are stronger than you know!','You are capable of amazing things!','Believe in yourself and you will be unstoppable!','You only fail when you stop trying!','You are doing exactly what you should be doing!','You are being so strong and patient!','I admire how strong you are!','No one is you and that is your superpower!','I cannot wait to see what you do next!']
+'encouraging':['A little progress each day adds up to big results!','You are stronger than you know!','You are capable of amazing things!','Believe in yourself and you will be unstoppable!','You are doing exactly what you should be doing!','You are being so strong and patient!','I admire how strong you are!','I cannot wait to see what you do next!','You are doing a great job!','You are getting better every day!','I am so proud of you!','You are on the right track!','You made it look easy!']
 }
 
